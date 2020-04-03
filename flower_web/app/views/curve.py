20c85fce5af4ed4c903c52c6302a2ff9ae79eb6e
nimin <nimin@konyo.com.cn>
2020-04-03 11:48:00 +0800

    finish show by date

--- flower_web/app/views/curve.py
+++ flower_web/app/views/curve.py
@@ -10,59 +10,67 @@ import datetime
 
 curve = Blueprint('curve', __name__)
 
+
 @curve.route("/air/", methods=["POST", "GET"])
 def air():
-	ret = {}
-	ret['code'] = 200
-	ret['success'] = True
-	ret['result'] = {}
+	if request.method == 'GET':
+		return render_template('curve.html')
+	if request.method == 'POST':
+		ret = {}
+		ret['code'] = 200
+		ret['success'] = True
+		ret['result'] = {}
 
-	data_str = request.get_data()
-	#app.logger.debug('recv POST: %s' % data_str)
-	print('recv POST: %s' % data_str)
-	data = json.loads(data_str.decode("utf-8"))
+		data_str = request.get_data()
+		logging.debug('recv POST: %s' % data_str)
+		data = json.loads(data_str.decode("utf-8"))
 
-	if request.method =='POST':
 		humidity = float(data.get('humidity'))
 		fahrenheit = float(data.get('fahrenheit'))
 		celsius = float(data.get('celsius'))
 		identification_id = int(data.get('id'))
 
 		if humidity and fahrenheit and celsius and identification_id:
-			identi = Identification.query.filter_by(id=identification_id).first()
+			identi = Identification.query.filter_by(
+				id=identification_id).first()
 			if not identi:
 				ret['success'] = False
-				logging.info('identification_id=%d not existed.' % identification_id)
+				logging.info('identification_id=%d not existed.' %
+							 identification_id)
 			else:
 				air = Air(humidity, fahrenheit, celsius, identification_id)
 				db.session.add(air)
 				db.session.commit()
 				logging.info('add air success.Air=%s' % air)
-	else:
-		begin_date = data.get("begin")
-		end_date = data.get("end")
 
-		end_datetime = datetime.datetime.combine(datetime.datetime.strptime(end_date,"%Y%m%d"), datetime.time(23, 59, 59))
-		begin_datetime = datetime.datetime.combine(datetime.datetime.strptime(begin_date,"%Y%m%d"), datetime.time(0, 0, 0))
+		return json.dumps(ret, ensure_ascii=False)
 
-		# airs = Air.query.filter(Air.update_date < end_datetime, Air.update_date > begin_datetime).all()
-		airs = db.session.query(func.date_formate(Air.update_date, '%Y-%m-%d').label('date'),
-		func.max(Air.humidity),
-		func.min(Air.humidity),
-		func.max(Air.celsius),
-		func.min(Air.celsius))
-		data = dict()
-		data['max_humidity'] = list()
-		data['min_humidity'] = list()
-		data['max_celsius'] = list()
-		data['min_celsius'] = list()
-		data['update_date'] = list()
-		for air in airs:
-			data['humidity'].append(air.humidity)
-			data['celsius'].append(air.celsius)
-			data['update_date'].append(air.update_date)
-		ret['result'] = data
-			
+@curve.route("/air/info", methods=["GET"])
+def info():
+	ret = {}
+	ret['code'] = 200
+	ret['success'] = True
+	ret['result'] = {}
+
+	airs = db.session.query(
+		func.max(Air.humidity).label('max_humidity'),
+		func.min(Air.humidity).label('min_humidity'),
+		func.max(Air.celsius).label('max_celsius'),
+		func.min(Air.celsius).label('min_celsius'),
+		func.date_format(Air.update_date, "%Y-%m-%d").label('date')).group_by(
+			func.date_format(Air.update_date, "%Y-%m-%d").label('date')).all()
+	data = dict()
+	data['max_humidity'] = list()
+	data['min_humidity'] = list()
+	data['max_celsius'] = list()
+	data['min_celsius'] = list()
+	data['update_date'] = list()
+	for air in airs:
+		data['max_humidity'].append(air.max_humidity)
+		data['min_humidity'].append(air.min_humidity)
+		data['max_celsius'].append(air.max_celsius)
+		data['min_celsius'].append(air.min_celsius)
+		data['update_date'].append(air.date)
+	ret['result'] = data
+
 	return json.dumps(ret, ensure_ascii=False)
- 
-
