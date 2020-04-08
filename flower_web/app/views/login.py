34151d23db57921141075d152004dea014c35c59
nimin <nimin@konyo.com.cn>
2020-04-08 19:53:58 +0800

    login turn up

--- flower_web/app/views/login.py
+++ flower_web/app/views/login.py
@@ -12,13 +12,14 @@ log_in = Blueprint('log_in', __name__)
 def login():
 	if request.method == 'GET':
 		return render_template('login.html')
-	if request.method == 'POST':
-		user_info = request.json
-		user = User.query.filter_by(name=user_info.get('name')).first()
+	else:
+		username = request.form.get('username')
+		usepwd = request.form.get('userpwd')
+		user = User.query.filter_by(name=username).first()
 		if not user:
-			logging.info('User:%s not exist' % user_info.get('name'))
+			logging.info('User:%s not exist' % username)
 			return jsonify(code=200, success=False, message='User not exist.', data={})
-		if user.pwd != user_info.get('pwd'):
+		if user.pwd != usepwd:
 			logging.info('Password is invalid')
 			return jsonify(code=200, success=False, message='Password is invalid.', data={})
 
@@ -30,7 +31,7 @@ def login():
 @log_in.route("/logout/")
 def logout():
 	logout_user()
-	logging.info('You were logged out')
+	logging.info('You were logged out.')
 	return redirect('/')
 
 
