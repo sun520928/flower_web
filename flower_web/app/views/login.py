4837b51efeab2ffd850dd9c6fce48a77d2f1329f
nimin <nimin@konyo.com.cn>
2020-04-08 20:22:25 +0800

    add flash show

--- flower_web/app/views/login.py
+++ flower_web/app/views/login.py
@@ -3,7 +3,7 @@ import json
 import logging
 
 from flask_login import login_required, login_user, logout_user
-from flask import Blueprint, request, render_template, redirect, jsonify
+from flask import Blueprint, request, render_template, redirect, jsonify, flash
 from app.models.user import User
 
 log_in = Blueprint('log_in', __name__)
@@ -17,21 +17,20 @@ def login():
 		usepwd = request.form.get('userpwd')
 		user = User.query.filter_by(name=username).first()
 		if not user:
-			logging.info('User:%s not exist' % username)
-			return jsonify(code=200, success=False, message='User not exist.', data={})
+			flash('User:%s not exist' % username)
+			return render_template('login.html')
 		if user.pwd != usepwd:
-			logging.info('Password is invalid')
-			return jsonify(code=200, success=False, message='Password is invalid.', data={})
+			flash('Password is invalid')
+			return render_template('login.html')
 
 		login_user(user)
-		logging.info('Logged in successfully.')
 		return render_template('curve.html')
 
 @login_required
 @log_in.route("/logout/")
 def logout():
 	logout_user()
-	logging.info('You were logged out.')
+	flash('You were logged out.')
 	return redirect('/')
 
 
