6cfdab329b3c5b79babf0af88559eccd2d20452f
sun520928 <sun520928@sohu.com>
2021-05-23 09:03:01 +0800

    add js for tab select

--- flower_web/app/views/main.py
+++ flower_web/app/views/main.py
@@ -1,13 +1,13 @@
 # -*- coding:utf-8 -*-
-from flask import Blueprint, request, render_template, redirect
+from flask import Blueprint, request, render_template, redirect, session
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 @main.route('/index/')
 def index():
-	return render_template('base.html')
-	# return render_template('login.html')
+	return render_template('base.html',username=session.get('username'))
+
 
 
 
