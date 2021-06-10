8321fa2004fd6fc4f4d938e6f497845de8abcd79
sun520928 <sun520928@sohu.com>
2021-06-10 09:07:59 +0800

    modify config

--- flower_web/config.py
+++ flower_web/config.py
@@ -1,5 +1,6 @@
 # -*- coding:utf-8 -*-
 import os
+from datetime import timedelta
 # from logging.config import dictConfig
 
 DB_USER = 'flower'
@@ -24,6 +25,41 @@ DB_PORT = '3306'
 # 	}
 # })
 
+'''
+      default_config = ImmutableDict(
+          {
+              "ENV": None,                    #虚拟环境，当前项目运行环境
+              "DEBUG": None,                  #debug模式的设置,开发环境用，自动重启项目，日志级别低，报错在前端显示具体代码
+              "TESTING": False,               #测试模式的设置，无限接近线上环境，不会重启项目，日志级别较高，不会在前端显示错误代码
+              "PROPAGATE_EXCEPTIONS": None,
+              "PRESERVE_CONTEXT_ON_EXCEPTION": None,
+              "SECRET_KEY": None,             #session秘钥配置
+              "PERMANENT_SESSION_LIFETIME": timedelta(days=31),   #session有效期时间的设置
+              "USE_X_SENDFILE": False,    
+              "SERVER_NAME": None,            #主机名设置
+              "APPLICATION_ROOT": "/",        #应用根目录配置
+              "SESSION_COOKIE_NAME": "session",   #cookies中存储的session字符串的键
+              "SESSION_COOKIE_DOMAIN": None,      #session作用域
+              "SESSION_COOKIE_PATH": None,        #session作用的请求路径
+              "SESSION_COOKIE_HTTPONLY": True,    #session是否只支持http请求方式
+              "SESSION_COOKIE_SECURE": False,     #session安全配置
+              "SESSION_COOKIE_SAMESITE": None,
+              "SESSION_REFRESH_EACH_REQUEST": True,
+              "MAX_CONTENT_LENGTH": None,
+              "SEND_FILE_MAX_AGE_DEFAULT": timedelta(hours=12),
+              "TRAP_BAD_REQUEST_ERRORS": None,
+              "TRAP_HTTP_EXCEPTIONS": False,
+              "EXPLAIN_TEMPLATE_LOADING": False,
+              "PREFERRED_URL_SCHEME": "http",
+              "JSON_AS_ASCII": True,
+              "JSON_SORT_KEYS": True,
+              "JSONIFY_PRETTYPRINT_REGULAR": False,
+              "JSONIFY_MIMETYPE": "application/json",     #设置jsonify响应时返回的contentype类型
+              "TEMPLATES_AUTO_RELOAD": None,
+              "MAX_COOKIE_SIZE": 4093,
+          }
+      )
+ '''
 
 class BaseConfig(object):
 	#SESSION_TYPE = 'redis'  # session类型为redis
@@ -38,19 +74,26 @@ class BaseConfig(object):
 	# 追踪对象的修改并且发送信号
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-	SECRET_KEY = '123456'
+	SECRET_KEY = os.urandom(24)
 	
 	DEBUG = True
 
 	SESSION_PERMANENT = True
 
+	PERMANENT_SESSION_LIFETIME = timedelta(days=1)
+
+	ENV = None
+
 class ProductionConfig(BaseConfig):
-	pass
+	ENV = 'Production'
+	DEBUG = False
 
 
 class DevelopmentConfig(BaseConfig):
-	pass
+	ENV = 'Development'
+	DEBUG = True
 
 
 class TestingConfig(BaseConfig):
-	pass
+	ENV = 'Testing'
+	TESTING = True
--- flower_web/manage.py
+++ flower_web/manage.py
@@ -1,13 +1,15 @@
 # -*- coding: utf-8 -*-
 from logging import debug
 import os
-from datetime import timedelta
 # from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from app import create_app, db
 from logs import setup_log
 from flask_login import LoginManager
 from app.models.user import User
+from config import TestingConfig
+
+
 
 loginmanager = LoginManager()
 loginmanager.session_protection = 'basic'
@@ -15,8 +17,7 @@ loginmanager.login_view = 'log_in.login'
 
 
 app = create_app()
-app.config['SECRET_KEY'] = os.urandom(24)
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
+app.config.from_object(TestingConfig)
 # manager = Manager(app)
 loginmanager.init_app(app)
 
